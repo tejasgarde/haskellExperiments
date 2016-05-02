8724d4c6099eaf0085eceb754dc8912deec3f431
tejasgarde <tejasgarde@yahoo.co.in>
2016-05-02 22:10:13 +0530

    few more functions

--- HaskellExperimets/functions.hs
+++ HaskellExperimets/functions.hs
@@ -424,7 +424,7 @@ nudge (Circle (Point x y) r) a b = Circle (Point (x+a) (y+b)) r
 nudge (Rectangle (Point x1 y1) (Point x2 y2)) a b 
 		= Rectangle (Point (x1+a) (y1+b)) (Point (x2+a) (y2+b))
 
-
+		
 
 -- Create the Basic SHapes
 
